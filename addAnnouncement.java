a26cd593d597c06f29fe184d7c4165b1b51d4849
sonalk878 <46801232+sonalk878@users.noreply.github.com>
2020-12-09 01:57:13 -0500

    Create addAnnouncement.java

---
+++ addAnnouncement.java
@@ -0,0 +1,14 @@
+package edu.gmu.cs477.fall2020.rythm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class addAnnouncement extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_announcement);
+    }
+}
