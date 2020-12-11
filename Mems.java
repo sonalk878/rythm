fa4bac54111b058e1afd77298305824bf1073c3e
sonalk878 <46801232+sonalk878@users.noreply.github.com>
2020-12-10 21:10:18 -0500

    Update Mems.java

--- Mems.java
+++ Mems.java
@@ -1,14 +1,64 @@
 package edu.gmu.cs477.fall2020.rythm;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.GridView;
+import android.widget.Toast;
+
+import com.afollestad.dragselectrecyclerview.DragSelectRecyclerView;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Mems extends AppCompatActivity {
+    GridView grid_view;
+    RecyclerView  mRecyclerView;
+    ImageAdapter mAdapter;
+    DatabaseReference mDatabaseRef;
+    List<Upload> mUploads;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mems);
+        mRecyclerView = findViewById(R.id.recycler_view);
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mUploads = new ArrayList<>();
+        mDatabaseRef = FirebaseDatabase.getInstance().getReference("uploads");
+        mDatabaseRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot postSnapshot: snapshot.getChildren()){
+                    Upload upload = postSnapshot.getValue(Upload.class);
+                    mUploads.add(upload);
+
+
+                }
+                mAdapter = new ImageAdapter(getBaseContext(), mUploads);
+                mRecyclerView.setAdapter(mAdapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(getBaseContext(), "Error getting images", Toast.LENGTH_SHORT).show();
+            }
+        });
+      /*  grid_view = findViewById(R.id.gridView);
+        grid_view.setPadding(5,0,25,0);
+        grid_view.setAdapter(new ImageAdapter(this));*/
+
     }
 }
